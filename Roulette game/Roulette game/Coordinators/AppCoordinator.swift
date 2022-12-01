05c7dc9aa09596978e7f69e3047fe85c16d900ad
Maksym Mykhalchuk <maks.mihalchuk.2000@gmail.com>
2022-12-01 22:48:32 +0200

    created AppCoordinator

--- Roulette game/Roulette game/Coordinators/AppCoordinator.swift
+++ Roulette game/Roulette game/Coordinators/AppCoordinator.swift
@@ -5,4 +5,31 @@
 //  Created by Максим Михальчук on 01.12.2022.
 //
 
-import Foundation
+import UIKit
+
+final class AppCoordinator: CoordinatorProtocol {
+    
+    var window: UIWindow
+    var childCoordinators: [CoordinatorProtocol]
+    var navController: UINavigationController
+    
+    init(window: UIWindow) {
+        self.childCoordinators = []
+        self.navController = UINavigationController()
+        self.window = window
+        self.window.rootViewController = navController
+        self.window.makeKeyAndVisible()
+    }
+    
+    func start() {
+
+    }
+    
+    private func authFlow() {
+        
+    }
+    
+    private func mainFlow() {
+        
+    }
+}
